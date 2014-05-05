df6e901ed06a1716fbaaf346fd9ba76442ea378d
truongnx42 <truongnx42@gmail.com>
2014-05-05 13:46:29 +0700

    SMS

--- sms_source/sms_source/Areas/admin/Models/sms_receive_log.cs
+++ sms_source/sms_source/Areas/admin/Models/sms_receive_log.cs
@@ -8,10 +8,10 @@ namespace sms_source.Areas.admin.Models
     public class sms_receive_log
     {
         public int id { get; set; }
-        public int sender_number { get; set; }
+        public string sender_number { get; set; }
         public int service_number { get; set; }
 
-        public string telcos { get; set; }
+        public int telcos { get; set; }
         public string command_code { get; set; }
         public DateTime sender_date { get; set; }
 
