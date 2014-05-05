899ab5ceea23fe34b5434b342b11decec80958f3
duytungnt92 <duytung190592@gmail.com>
2014-05-05 19:23:50 +0700

    h

---
+++ sms_source/sms_source/Areas/admin/Controllers/APIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace sms_source.Areas.admin.Controllers
+{
+    public class APIController : Controller
+    {
+        //
+        // GET: /admin/API/
+
+        [HttpPost]
+        public ActionResult abc(FormCollection colect)
+        {
+            
+            return View();
+        }
+
+        public ActionResult Index() { 
+            XElement xBookParticipants =  new XElement("BookParticipants",  
+                                            new XElement("BookParticipant",  
+                                                new XAttribute("type", "Author"), 
+                                                    new XElement("FirstName", "Joe"), 
+                                                    new XElement("LastName", "Rattz")), 
+                                            new XElement("BookParticipant",       
+                                                 new XAttribute("type", "Editor"), 
+                                                    new XElement("FirstName", "Ewan"), 
+                                                    new XElement("LastName", "Buckingham"))); 
+            return Content(xBookParticipants.ToString() , "text/xml");
+        }
+
+    }
+}
